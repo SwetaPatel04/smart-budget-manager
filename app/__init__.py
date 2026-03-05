db4b6f428e976b7eb6a77229e5d1162470b9b9e2
Sweta <sweta48etava@g202186424+SwetaPatel04@users.noreply.github.com>
2026-03-05 15:52:49 -0500

    SBM-4 frontend dashboard with charts and ml predictions complete

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from config import Config
+import os
 
 # Initialize extensions
 db = SQLAlchemy()
@@ -10,8 +11,11 @@ jwt = JWTManager()
 bcrypt = Bcrypt()
 
 def create_app():
+    # Tell Flask where templates folder is
+    template_dir = os.path.abspath('templates')
+    
     # Create Flask app
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder=template_dir)
     
     # Load config
     app.config.from_object(Config)
@@ -24,9 +28,11 @@ def create_app():
     # Register blueprints (routes)
     from app.routes.auth import auth_bp
     from app.routes.expenses import expenses_bp
+    from app.routes import main_bp
     
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(expenses_bp, url_prefix='/api/expenses')
+    app.register_blueprint(main_bp)
     
     # Create database tables
     with app.app_context():
